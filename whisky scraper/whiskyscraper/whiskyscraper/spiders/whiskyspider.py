0b0557776a4407c06e08f1512ca5f8e080c464eb
Ashutosh <ashutoshb2000@gmail.com>
2022-04-25 13:44:12 +0100

    item containers

--- whisky scraper/whiskyscraper/whiskyscraper/spiders/whiskyspider.py
+++ whisky scraper/whiskyscraper/whiskyscraper/spiders/whiskyspider.py
@@ -1,10 +1,12 @@
 import scrapy
+from ..items import WhiskyscraperItem
 
 class WhiskyScraper(scrapy.Spider):
     name = 'whisky_spider'
     start_urls = ['https://www.whiskyshop.com/scotch-whisky?item_availability=In+Stock']
 
     def parse(self, response):
+        items = WhiskyscraperItem()
         for products in response.css('div.product-item-info'):
             name = products.css('a.product-item-link::text').get()
             link = products.css('a.product-item-link').attrib['href']
@@ -12,12 +14,14 @@ class WhiskyScraper(scrapy.Spider):
                 price = products.css('span.price::text').get().replace('£', '')
             except:
                 price = 'sold out'
-            yield {
-                'name': name,
-                'price': price,
-                'link': link,
-            }
+
+            items['name'] = name
+            items['price'] = price
+            items['link'] = link
+
+            yield items
 
         next_page = response.css('a.action.next').attrib['href']
         if next_page:
             yield response.follow(next_page, callback=self.parse)
+            
